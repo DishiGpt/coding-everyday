55145221bd4a1727a6638fcdb74e5102d6314f5b
Dishi Gupta <dishigpt15@gmail.com>
2025-02-16 20:01:38 +0530

    Add files via upload

---
+++ ques1.cpp
@@ -0,0 +1,20 @@
+// Add 2 numbers
+#include <iostream>
+using namespace std;
+void add();
+int main() {
+    // Write C++ code here
+    add();
+
+    return 0;
+}
+void add(){
+    cout<<"enter 2 numbers :"<<endl;
+    int num1,num2;
+    cout<<"number 1 ";
+    cin>>num1;
+    cout<<"number 2 ";
+    cin>>num2;
+    cout<<"sum of the numbers is "<<num1+num2;
+   
+} 
