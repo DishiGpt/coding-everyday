bb4ef75b2c632fc3485d523f57f74e547aa3decf
Dishi Gupta <dishigpt15@gmail.com>
2025-03-07 19:49:16 +0530

    Add files via upload

--- ques3.cpp
+++ ques3.cpp
@@ -1,22 +1,21 @@
-#include<iostream>
-using namespace std;
-class person{
-    public :
-    string name;
-    int age;
-    void display()
-    {
-        cout<<"the details of the person are "<<name<<" and age is "<<age;
-    }
-};
-
-int main()
-{
-    person person1;
-    cout<<"enter the name ";
-    cin>>person1.name;
-    cout<<"enter age ";
-    cin>>person1.age;
-    person1.display();
-    return 0;
+#include <iostream> 
+using namespace std; 
+class Artist { 
+public: 
+void paint() { cout << "Painting a masterpiece." << endl; } 
+}; 
+class Engineer { 
+public: 
+void build() { cout << "Building a structure." << endl; } 
+}; 
+class Innovator : public Artist, public Engineer { 
+public: 
+void createInnovation() { cout << "Creating an innovation." << endl; } 
+}; 
+int main() { 
+Innovator i; 
+i.paint(); 
+i.build(); 
+i.createInnovation(); 
+return 0; 
 }
