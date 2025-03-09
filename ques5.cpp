d3af923fce7e3eb38559761dd8fc2a4b8ad0a4b7
Dishi Gupta <dishigpt15@gmail.com>
2025-03-09 13:19:01 +0530

    Add files via upload

--- ques5.cpp
+++ ques5.cpp
@@ -1,26 +1,25 @@
-#include <iostream>
-using namespace std;
-class circle{
-    public:
-    int rad;
+#include <iostream> 
+using namespace std; 
+class Shape { 
+public: 
+virtual void area() { cout << "Undefined area." << endl; } 
+}; 
+class Circle : public Shape { 
+public: 
+void area() { cout << "Area of Circle: πr²." << endl; } 
 
-    float area(int rad1);
-
-};
-
-float circle::area(int rad)
-{
-    float ar=3.14*rad*rad;
-    return ar;
-}
-
-int main()
-{
-    circle cir1;
-    int rad;
-    cout<<"enter the radius of the circle : ";
-    cin>>rad;
-    cout<<"the area of the circle is : "<<cir1.area(rad);
-    return 0;
-
+}; 
+class Rectangle : public Shape { 
+public: 
+void area() { cout << "Area of Rectangle: l × w." << endl; } 
+}; 
+int main() { 
+Shape *s; 
+Circle c; 
+Rectangle r; 
+s = &c; 
+s->area(); 
+s = &r; 
+s->area(); 
+return 0; 
 }
