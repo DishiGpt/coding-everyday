33cf72e69b5152a0f1f3c3f88b22b5636df05a7b
Dishi Gupta <dishigpt15@gmail.com>
2025-03-10 18:31:23 +0530

    Add files via upload

--- ques7.cpp
+++ ques7.cpp
@@ -1,15 +1,25 @@
-#include<iostream>
-using namespace std;
-struct book{
-    string title;
-    string author;
-    float price;
-};
+#include <iostream> 
+using namespace std; 
+class Appliance { 
+public: 
+virtual void powerUsage() = 0; // Pure virtual function 
+}; 
+class Fan : public Appliance { 
+public: 
+void powerUsage() { cout << "Fan uses 70W." << endl; } 
+}; 
+class WashingMachine : public Appliance { 
+public: 
+void powerUsage() { cout << "Washing Machine uses 500W." << endl; } 
+}; 
+int main() { 
+Appliance *a; 
+Fan f; 
+WashingMachine wm; 
 
-int main()
-{
-    book book1={"Da Vinci Code","Dan Brown",500};
-    cout<<"the book name is "<<book1.title<<endl<<"the author is  "<<book1.author<<endl<<"price "<<book1.price;
-    return 0;
-
+a = &f;
+a->powerUsage(); 
+a = &wm; 
+a->powerUsage(); 
+return 0; 
 }
