00b09f2e9897b2bfb3d9539aab62847891b5f76f
Dishi Gupta <dishigpt15@gmail.com>
2025-04-01 19:19:04 +0530

    Add files via upload

---
+++ power_set.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    vector<int> nums;
+    vector<vector<int>> powerset;
+    int n;
+    cout<<"Enter the number of elements: ";
+    cin>>n;
+    int ts=1<<n;
+    for(int i=0;i<n;i++){
+        int x;
+        cout<<"Enter the element: ";
+        cin>>x;
+        nums.push_back(x);
+    }
+    for(int i=0;i<ts;i++){
+        vector<int> temp;
+        for(int j=0;j<n;j++){
+            if(i & (1<<j)){
+                temp.push_back(nums[j]);
+            }
+        }
+        powerset.push_back(temp);
+    }
+    for(int i=0;i<powerset.size();i++){
+        for(int j=0;j<powerset[i].size();j++){
+            cout<<powerset[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
